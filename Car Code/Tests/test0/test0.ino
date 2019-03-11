06aaa258468733cda5fa37030f38dae9786ee4f3
sherifiresh <firesh@hotmail.com>
2019-03-11 20:50:11 +0200

    test done

--- Car Code/Tests/test0/test0.ino
+++ Car Code/Tests/test0/test0.ino
@@ -1,3 +1,5 @@
+bool f = false;
+int i = 15;
 void setup() {
   // put your setup code here, to run once:
   pinMode(8, OUTPUT);
@@ -5,6 +7,13 @@ void setup() {
 }
 
 void loop() {
+  if(i > 0)
+  {
   digitalWrite(8, HIGH);
+  delay(100);
+  digitalWrite(8, LOW);
+  delay(100);
+  i--;
+  }
 
 }
